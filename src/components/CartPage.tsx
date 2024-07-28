3278b97fe078bd7e015ca376fd4b4d6ff3741788
tarasov.d <d.tarasov@softmedialab.com>
2024-07-29 02:11:32 +0700

    fix

--- src/components/CartPage.tsx
+++ src/components/CartPage.tsx
@@ -9,23 +9,22 @@ const CartPage = () => {
   };
 
   const cart = useAppSelector((state) => state.cart);
-  const sis = useAppSelector(state => state.sizes)
-  console.log(sis, 'siz')
   const products = useAppSelector((state) => state.products);
+
   return cart?.length ? (
     <>
       <div className="product-cart">
         {cart.map((cartProduct, index) => {
-          const { name } = products.find(
+          const product = products.find(
             ({ id }) => id === cartProduct.productId
           );
-          if (!name) return null;
+          if (!product?.name) return null;
           return (
             <CartItem
               key={index}
               cartProduct={cartProduct}
               handlerClick={deleteProd}
-              productName={name}
+              productName={product.name}
             />
           );
         })}
