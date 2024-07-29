d1838505e6ff454338a65b1b51e7f37995f792d8
tarasov.d <d.tarasov@softmedialab.com>
2024-07-29 13:50:48 +0700

    fix3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
 import Button from "./UI/Button";
 import { Link } from "react-router-dom";
-
+import { useAppSelector } from "../hooks";
 const Header = () => {
+  const cart = useAppSelector(state => state.cart)
+
   return (
     <div className="header">
       <Link to={"/"}>
         <Button>Каталог</Button>
       </Link>
       <Link to={"/cart"}>
-        <Button>Корзина</Button>
+        <Button><p>{`Корзина   ${cart.length}`}</p></Button>
       </Link>
     </div>
   );
