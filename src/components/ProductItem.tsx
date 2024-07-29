2c53ef3a7e8a5f8ea38d5c1b15565d9be1336d41
tarasov.d <d.tarasov@softmedialab.com>
2024-07-29 13:57:21 +0700

    fix4

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -9,7 +9,7 @@ const ProductItem = ({ product}: ProductItem) => {
   return (
     <div className="product-card">
       <Link to={`/product/${product?.id}`}>
-        <Button>
+        <Button className="product-item">
           <img
             src={product?.colors[0].images[0]}
             className="product-card__img"
