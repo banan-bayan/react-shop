f14466150ca548b8704cbe1916259a9151e93aa4
tarasov.d <d.tarasov@softmedialab.com>
2024-07-30 16:07:09 +0700

    localeStorage_add

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,8 +1,15 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CartItemInterface } from "../Types";
 
+const getInitial = () => {
+  const cartFromStaorage = localStorage.getItem("cart");
+  if (!cartFromStaorage) return [];
 
-const initialState: CartItemInterface[] = [];
+  return JSON.parse(cartFromStaorage);
+};
+
+const initialState: CartItemInterface[] = getInitial();
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -13,11 +20,12 @@ const cartSlice = createSlice({
       );
       if (isHaveProduct) {
         state.push(actions.payload);
-
       }
+      localStorage.setItem("cart", JSON.stringify(state));
     },
     deleteProduct(state, actions: PayloadAction<{ cartId: string }>) {
       state = state.filter(({ cartId }) => cartId !== actions.payload.cartId);
+      localStorage.setItem("cart", JSON.stringify(state));
       return state;
     },
   },
